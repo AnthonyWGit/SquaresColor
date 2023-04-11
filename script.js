330a66dff3273056d3aea2f714ccd2db758efc12
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-04-11 16:48:31 +0200

    cleaning, comments, functions to top

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-const wrapperQuerySelector = document.querySelector(".wrapper")
-const squareQuerySelector = document.querySelector(".square")
-const newSquareCreateDiv = document.createElement("div")
-document.addEventListener("keydown", squareCreation)          //Will pop up squares ib the screen
-document.addEventListener("keydown", squareDeletion)
-document.addEventListener("keydown", createAll)
-let randomization 
-let lastNumber
-let cellsEmoji = Range(6,10).concat(Range(19,20)).concat(Range(26,27)).concat(33).concat(43).concat(47).concat(59).concat(62).concat(74).concat(Range(76,90)).concat(91)
-                .concat(Range(93,94)).concat(Range(97,100)).concat(103).concat(Range(105,106)).concat(Range(108,110)).concat(112).concat(Range(114,116)).concat(118).concat(Range(120,121))
-                .concat(Range(124,126)).concat(Range(130,132)).concat(Range(135,136)).concat(150).concat(152).concat(155).concat(164).concat(167).concat(Range(171,174)).concat(179)
-                .concat(183).concat(193).concat(Range(199,200)).concat(Range(206,207)).concat(Range(216,220))
-console.log(cellsEmoji)
-
 function squareCreation(event)   
 {
     if (event.keyCode == 38)
@@ -56,7 +42,6 @@ function squareDeletion(event)                                                  
         if (wrapperQuerySelector.children.length !== 0) wrapperQuerySelector.removeChild(wrapperQuerySelector.lastChild) //We use lastChild to target the last square we created by clicking        
     }
 }
-
 function createAll(event)
 {
     if (event.keyCode == 39)
@@ -95,25 +80,37 @@ function createAll(event)
         }        
     }
 }
-
 function turnBlackOnClick()
 {
-    cellsEmoji.forEach(element => {
+    cellsEmoji.forEach(element => {                         //Foreach so we can go through all the numbers is the huge range we created 
 
-    if (this == wrapperQuerySelector.children[element])
+    if (this == wrapperQuerySelector.children[element - 1]) //element - 1 because there is a "" in the nodelist created when the first square pops 
     {
         this.classList.remove("blue")           //Remove color classes 
         this.classList.remove("violet")
         this.classList.remove("silver")
         this.classList.remove("grey")
         this.classList.add("black")
-        alert("aaa")        
     }
         
     });
-
 }
 function Range(beginRange, endRange)    //This function will let us build ranges for arrays containing emoji cells 
 {
     return Array.from(Array(endRange - beginRange + 1).keys()).map( x => beginRange + x)     // +1 because index starts at 0 and we'll miss one number without it. We give the beginning and the end of array 
-}                                                                                           //  we want. Array.from always starts at 0 so we use .map to transform the initial array 
+}
+//______________________________________________________________DOM selectors__________________________________
+const wrapperQuerySelector = document.querySelector(".wrapper")
+const squareQuerySelector = document.querySelector(".square")
+const newSquareCreateDiv = document.createElement("div")
+//_______________________________________________________________Event Listeners______________________________________
+document.addEventListener("keydown", squareCreation)          //Will pop up squares ib the screen
+document.addEventListener("keydown", squareDeletion)
+document.addEventListener("keydown", createAll)
+//_________________________________________________________________Variables________________________________________________
+let randomization 
+let lastNumber
+let cellsEmoji = Range(6,10).concat(Range(19,20)).concat(Range(26,27)).concat(33).concat(43).concat(47).concat(59).concat(62).concat(74).concat(Range(76,90)).concat(91)
+                .concat(Range(93,94)).concat(Range(97,100)).concat(103).concat(Range(105,106)).concat(Range(108,110)).concat(112).concat(Range(114,116)).concat(118).concat(Range(120,121))
+                .concat(Range(124,126)).concat(Range(130,132)).concat(Range(135,136)).concat(150).concat(152).concat(155).concat(164).concat(167).concat(Range(171,174)).concat(179)
+                .concat(183).concat(193).concat(Range(199,200)).concat(Range(206,207)).concat(Range(216,220))                                                                                           //  we want. Array.from always starts at 0 so we use .map to transform the initial array 
